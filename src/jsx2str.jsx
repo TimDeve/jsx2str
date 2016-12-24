0ce092f400f350a75847e4af2ef8b28bca86caf1
Timothee Deve <timdeve@users.noreply.github.com>
2016-12-24 18:20:09 +0100

    Naive implementation

--- src/jsx2str.jsx
+++ src/jsx2str.jsx
@@ -1,5 +1,55 @@
-function jsx2src() {
-  console.log("jsx2src")
+function jsx2src(tag, attr, ...content) {
+  if (!content) {
+    if (/<.+>\w/.test(tag)) {
+      return tag
+    }
+    else {
+      let strArr = []
+      strArr.push(`<${tag}`)
+
+      for (let key in attr) {
+         let attrName = ""
+         switch(key) {
+           case "className":
+             attrName = "class"
+             break
+           case "htmlFor":
+             attrName = "for"
+             break
+           default:
+             attrName = key
+         }
+         strArr.push(` ${attrName}="${attr[key]}"`)
+      }
+
+      strArr.push(">")
+      return strArr.join("")
+    }
+  }
+  else if (content) {
+    let strArr = []
+    strArr.push(`<${tag}`)
+
+    for (let key in attr) {
+      let attrName = ""
+      switch(key) {
+        case "className":
+          attrName = "class"
+          break
+        case "forHtml":
+          attrName = "for"
+          break
+        default:
+          attrName = key
+      }
+      strArr.push(` ${attrName}="${attr[key]}"`)
+    }
+
+    strArr.push(">")
+    strArr.push(...content)
+    strArr.push(`</${tag}>`)
+    return strArr.join("")
+  }
 }
 
 export default jsx2src
