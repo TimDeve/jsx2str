61d2ac7b182d2ee651e5227e65d2e01e45b7a8ce
Timothee Deve <timdeve@users.noreply.github.com>
2017-01-06 12:27:24 +0100

    Makes things DRYer by looping for the attributes only once

--- src/jsx2str.jsx
+++ src/jsx2str.jsx
@@ -1,46 +1,30 @@
 function jsx2src(tag, attr, ...content) {
-  if (content.length === 0) {
-    if (typeof tag === "function") {
-      return tag(attr)
-    }
-    else {
-      let strAttr = []
-      for (let key in attr) {
-         let attrName = ""
-         switch(key) {
-           case "className":
-             attrName = "class"
-             break
-           case "htmlFor":
-             attrName = "for"
-             break
-           default:
-             attrName = key
-         }
-         strAttr.push(` ${attrName}="${attr[key]}"`)
-      }
-
-      return strArr.push(`<${tag} ${strAttr.join("")}>`)
-    }
+  if (typeof tag === "function") {
+    return tag(attr)
   }
-  else if (content.length !== 0) {
+  else {
     let strAttr = []
     for (let key in attr) {
-      let attrName = ""
-      switch(key) {
-        case "className":
-          attrName = "class"
-          break
-        case "forHtml":
-          attrName = "for"
-          break
-        default:
-          attrName = key
-      }
-      strAttr.push(` ${attrName}="${attr[key]}"`)
+       let attrName = ""
+       switch(key) {
+         case "className":
+           attrName = "class"
+           break
+         case "htmlFor":
+           attrName = "for"
+           break
+         default:
+           attrName = key
+       }
+       strAttr.push(` ${attrName}="${attr[key]}"`)
     }
 
-    return `<${tag} ${strAttr.join("")}>${content.join("")}</${tag}>`
+    if (content.length === 0) {
+      return `<${tag} ${strAttr.join("")}>`
+    }
+    else {
+      return `<${tag} ${strAttr.join("")}>${content.join("")}</${tag}>`
+    }
   }
 }
 
